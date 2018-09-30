309d415bf011a9c7184c57af6492dc4cfe9b4ec2
agrojas <rojasagustin90@gmail.com>
2018-09-30 11:28:33 -0300

    Se agrega la logica de los colores en los botones verdadero y falso

--- js/service/trivias.js
+++ js/service/trivias.js
@@ -1,7 +1,6 @@
 var correctAnswers = []
 
 var currentQuestion = null;
-var currentResponse = null;
 var currentPoints = 0;
 var numberOfAnswers  = 0;
 
@@ -12,35 +11,47 @@ function loadQuestion() {
 	$("#legal-question").text(currentQuestion['assertion']);
 	$("#legal-question-info").text(currentQuestion['info']);
 	$("#legal-question-info").css('display', 'none');
+	
+	$("#legal-true").css('color','black')	
+	$("#legal-true").removeAttr('disabled');
+	
+	$("#legal-false").css('color','black')
+	$("#legal-false").removeAttr('disabled');
+	
 }
 
 
-$( "#legal-trivia" ).ready(function() {
+$("#legal-trivia").ready(function() {
 	loadQuestion()
 });
 
-$( "#legal-next-btn" ).click(function() {
+$("#legal-next-btn").click(function() {
 	loadQuestion()
 });
 
 
-$( "#legal-true" ).click(function() {
+$("#legal-true").click(function() {
 	currentResponse = "VERDADERO";
 	$("#legal-question-info").css('display', 'block');
-	checkAnswer()	
+	checkAnswer(currentResponse,"#legal-true")	
 });
 
 
 $( "#legal-false" ).click(function() {
 	currentResponse = "FALSO";
 	$("#legal-question-info").css('display', 'block');
-	checkAnswer()
+	checkAnswer(currentResponse,"#legal-false")
 });
 
 
-function checkAnswer(){
+function checkAnswer(currentResponse, selectedBtn){
 	if (currentQuestion['answer'] == currentResponse) {
 		currentPoints += 10;
+		$(selectedBtn).css('color','green');
+	} else {
+		$(selectedBtn).css('color','red');
 	}
+	$("#legal-false").attr('disabled','disabled');
+	$("#legal-false").attr('disabled','disabled');
 
 }
